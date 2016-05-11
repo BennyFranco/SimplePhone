6024ed34d9ee55a8db0085702327872940a7520f
Benny Franco Dennis <bennyfranco@sandcode.com>
2016-05-11 08:45:39 -0700

    Home screen updated

--- app/src/main/java/itson/mx/simplephone/HomeActivity.java
+++ app/src/main/java/itson/mx/simplephone/HomeActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 
 /**
@@ -21,43 +20,6 @@ public class HomeActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         manager = getPackageManager();
-
-        Button btnLlamar = (Button) findViewById(R.id.btnLlamar);
-        Button btnMensaje = (Button) findViewById(R.id.btnMensaje);
-        Button btnFotos = (Button) findViewById(R.id.btnFotos);
-        Button btnCamara = (Button) findViewById(R.id.btnFoto);
-
-        btnLlamar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = manager.getLaunchIntentForPackage("com.android.contacts");
-                startActivity(i);
-            }
-        });
-
-        btnMensaje.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = manager.getLaunchIntentForPackage("com.android.mms");
-                startActivity(i);
-            }
-        });
-
-        btnFotos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = manager.getLaunchIntentForPackage("com.sec.android.gallery3d");
-                startActivity(i);
-            }
-        });
-
-        btnCamara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = manager.getLaunchIntentForPackage("com.sec.android.app.camera");
-                startActivity(i);
-            }
-        });
     }
 
     public void showApps(View v){
